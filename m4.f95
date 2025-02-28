dc31901e1fb73f94ea48fd7c86973400c915b095
azizikri <azizikri@gmail.com>
2025-03-01 06:46:05 +0700

    enhance m4 program with detailed output for circle calculations and added utility functions

--- m4.f95
+++ m4.f95
@@ -7,34 +7,41 @@ program m4
     x = 5.0
     y = 10.0
 
+    print *, "====== Subroutine ======"
     call calculate_circle(radius, area, circumference)
 
     print '(A,F7.2)', "Radius: ", radius
     print '(A,F7.2)', "Area: ", area
     print '(A,F7.2)', "Circumference: ", circumference
 
+
+    print *, "====== Function ======"
+    print *, "Average:", average(x, y)
+    print *, "Maximum:", maximum(x, y)
 contains
+    ! Subroutine bisa in, out, atau inout
     subroutine calculate_circle(r, a, c)
         implicit none
-        real, intent(in) :: r
-        real, intent(out) :: a, c
+        real, intent(in) :: r !in adalah parameter yang hanya bisa dibaca
+        real, intent(out) :: a, c !out adalah parameter yang hanya bisa ditulis
         real, parameter :: pi = 3.14159
 
         a = pi * r**2
         c = 2.0 * pi * r
     end subroutine calculate_circle
 
-    function average(a, b) result(avg)
+    ! Function biasanya hanya bisa in
+    function average(a, b) result(avg) !result adalah value yang direturn
         implicit none
-        real, intent(in) :: a, b
-        real :: avg
+        real, intent(in) :: a, b !in adalah parameter yang hanya bisa dibaca
+        real :: avg !result yang direturn harus dideklarasikan
 
         avg = (a + b) / 2.0
     end function average
 
     function maximum(a, b) result(max_val)
         implicit none
-        real, intent(in) :: a, b
+        real, intent(in) :: a, b !in adalah parameter yang hanya bisa dibaca
         real :: max_val
 
         if (a > b) then
